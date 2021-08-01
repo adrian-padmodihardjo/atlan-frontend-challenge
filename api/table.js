af652678ad7e76ee39670cba8d9aab69026f2451
adrian-padmodihardjo <adrian.pdmh@gmail.com>
2021-08-01 16:57:03 +0700

    api: create GET request methods for table headers and table data

---
+++ api/table.js
@@ -0,0 +1,21 @@
+import { request } from '@/api-client/main'
+
+export function getTableHeaders (tableName) {
+  return request.call(this, {
+    method: 'get',
+    url: '/table-headers',
+    params: {
+      table: tableName,
+    },
+  })
+}
+
+export function getTableData (tableName) {
+  return request.call(this, {
+    method: 'get',
+    url: `/feature/${tableName}`,
+    params: {
+      table: tableName,
+    },
+  })
+}
