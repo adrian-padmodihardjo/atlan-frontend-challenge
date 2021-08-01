f2109e5d54b2539cff4dbff76ee5e838a8d073f2
adrian-padmodihardjo <adrian.pdmh@gmail.com>
2021-08-01 19:08:25 +0700

    insert query result table on TheEditor layout

--- components/Layouts/TheEditor/TheEditor.vue
+++ components/Layouts/TheEditor/TheEditor.vue
@@ -1,14 +1,25 @@
 <template>
   <section class="the-editor">
-    <MyAceEditor />
+    <MyAceEditor class="the-editor__ace" />
+    <QueryResultTable
+      class="the-editor__query-result-table"
+      :table-name="tableName"
+    />
   </section>
 </template>
 
 <script>
 import { MyAceEditor } from '@/components/MyAceEditor'
+import { QueryResultTable } from '@/components/QueryResultTable'
 export default {
   components: {
     MyAceEditor,
+    QueryResultTable,
+  },
+  data () {
+    return {
+      tableName: 'customers',
+    }
   },
 }
 </script>
